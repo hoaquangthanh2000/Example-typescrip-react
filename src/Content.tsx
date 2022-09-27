c617d579aa38bca0383bba34fd2acb435e4b11f6
quangthanh <hoaquangthanh2000@gmail.com>
2022-09-27 11:17:29 +0700

    example2-27/9

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,27 +1,41 @@
 
 import { useEffect, useState } from "react"
 
+const tabs = ['comments','posts','albums']
 function Content(){
     const [title,setTitle] = useState('')
     const [todos,setTodos] = useState<{userId:number,id:number,title:string,completed:boolean}[]>([])
+    const [type,setType] = useState('posts')
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
             .then(res => res.json())
-            .then(todos => {
-                setTodos(todos)
+            .then(items => {
+                setTodos(items)
             })
-    },[])
+    },[type])
 
 
     return (
         <div>
+            {tabs.map((tab) => (
+                <button 
+                    key={tab}
+                    style={ type === tab ? {
+                        color:'#fff',
+                        backgroundColor:'#333'
+                    }:{}}
+                    onClick={() => setType(tab)}
+                >
+                    {tab}
+                </button>
+            ))}
             <input
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
             />
             <ul>
-                {todos.map((todo,index) => (
-                    <li key={index}>{todo.title}</li>
+                {todos.map((item,index) => (
+                    <li key={index}>{item.title}</li>
                 ))}
             </ul>
         </div>
