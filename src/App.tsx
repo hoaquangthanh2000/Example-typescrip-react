0a9067d92a6eb3c1b444805ab0201d5dd75b86a2
quangthanh <hoaquangthanh2000@gmail.com>
2022-09-27 10:54:15 +0700

    example1-27/9

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Content from './Content';
 
-function App() {
+
+function App() { 
+  const [show,setShow] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding:32}}>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <Content/>}
     </div>
   );
 }
